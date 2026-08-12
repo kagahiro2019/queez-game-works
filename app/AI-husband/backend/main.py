464bf8f27e8c68dc6a655871cdbfc3c90ab00961
kagahiro <kagahiro2019@gmail.com>
2026-08-12 16:32:00 +0900

    main.py　に　GitHub Page　を追加

--- app/AI-husband/backend/main.py
+++ app/AI-husband/backend/main.py
@@ -49,6 +49,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
+        "https://kagahiro2019.github.io",
     ],
     allow_credentials=True,
     allow_methods=["*"],
